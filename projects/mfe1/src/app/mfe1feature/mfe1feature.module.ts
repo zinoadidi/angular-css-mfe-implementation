ac422754f0aae49b7cf8b3223af844201cb88bfa
Zino Adidi <zino.adidi@btw.ee>
2022-11-28 11:17:02 +0200

    initial commit

---
+++ projects/mfe1/src/app/mfe1feature/mfe1feature.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Mfe1featureRoutingModule } from './mfe1feature-routing.module';
+import { Mfe1featureComponent } from './mfe1feature.component';
+
+
+@NgModule({
+  declarations: [
+    Mfe1featureComponent,
+  ],
+  exports: [
+    Mfe1featureComponent,
+  ],
+  imports: [
+    CommonModule,
+    Mfe1featureRoutingModule,
+  ],
+})
+export class Mfe1featureModule { }
